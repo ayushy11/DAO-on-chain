7f1675306953cf8f010e930e1f46ef099bbde157
AYUSH YADAV <ayushexx@gmail.com>
2022-05-24 18:46:43 +0530

    governor contract

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+// We want to wait for a new vote to be "executed"
+
+// Proposal - Everyone who holds the governance token has to pay 5 tokens
+
+// Give time to users to "get out" if they don't like governance update
+
 contract TimeLock {
 
 }

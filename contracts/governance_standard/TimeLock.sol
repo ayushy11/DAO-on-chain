a98a298d489474a82c22ed97f9c8d28c8f3c483e
AYUSH YADAV <ayushexx@gmail.com>
2022-05-24 18:57:44 +0530

    timelock contract

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-// We want to wait for a new vote to be "executed"
+import "@openzeppelin/contracts/governance/TimelockController.sol";
 
-// Proposal - Everyone who holds the governance token has to pay 5 tokens
-
-// Give time to users to "get out" if they don't like governance update
-
-contract TimeLock {
-
+contract TimeLock is TimelockController {
+    // minDelay: how long you have to wait before executing
+    // proposers: list of addresses that can propose
+    // executors: who can execute when a proposal passes
+    constructor (
+        uint256 minDelay, 
+        address[] memory proposers, 
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors) 
+    {}
 }

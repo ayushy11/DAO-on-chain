ec0b3a12ccf837d48eebe4c3fca8662a2333e478
AYUSH YADAV <ayushexx@gmail.com>
2022-05-26 13:51:13 +0530

    deploy-time-lock

--- deploy/02-deploy-time-lock.ts
+++ deploy/02-deploy-time-lock.ts
@@ -1,5 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { MIN_DELAY } from "../helper-hardhat-config";
 
 const deployTimeLock: DeployFunction = async (
   hre: HardhatRuntimeEnvironment
@@ -8,5 +9,13 @@ const deployTimeLock: DeployFunction = async (
   const { getNamedAccounts, deployments, network } = hre;
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
-  
+  log("Deploying Timelock...");
+
+  const timelock = await deploy("TimeLock", {
+    from: deployer,
+    args: [MIN_DELAY, [], []],
+    log: true,
+  });
 };
+
+export default deployTimeLock;
